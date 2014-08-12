c1bcd10886c28715ab6a82ab30eb524c53560c98
kohoutLearning <kohout07@gmail.com>
2014-08-12 13:23:50 -0700

    update

--- script2.js
+++ script2.js
@@ -1,48 +1,153 @@
 $(document).ready();
 
-$(function() {
+// Empty model
+var ListItemModel = Backbone.Model.extend({});
 
-ToDoList = Backbone.Collection.extend({
-    initialize: function(){
+// Empty collection; associate model and collection
+var ListItemCollection = Backbone.Collection.extend({
+   model: ListItemModel
+});
 
+// Create an instance of the collection
+var listItems = new ListItemCollection();
+
+// Generic view to create a list item
+var ListItemView = Backbone.View.extend({
+    //el:'div',
+    
+   // This view will expect parameters to be passed in
+   initialize: function(options) {
+      // Save the options for later use
+      this.options = options || {};      
+   },
+   
+    events: {
+      'click':'removeItem'
+    },   
+    
+    removeItem: function() {
+      this.remove();
+    },
+    
+   render: function() {
+      // Get the model that was passed in to this view
+      var newestItem = this.options.model;
+      
+      console.log('ListItemView.rendered');
+      
+      this.$el.html(newestItem.get("itemName"));
+      
+      return this;
+   }
+});
+
+// View for the existing 'Add Button' to attach an event
+// and handle the event
+var AddButtonView = Backbone.View.extend({
+    el:'#add-item-button',
+    
+    events: {
+        'click':'addItem'
+    },
+        
+    addItem: function() {
+      //alert("AddButtonView.addItem");
+        // Get the new item from the text input box using
+        // jQuery function .val()
+        var newItem = $('#toDo-Item').val();
+        //alert("newItem = " + newItem);
+        // Create a new list item model with the new item name
+        var item = new ListItemModel({itemName: newItem});
+        console.log(item.get("itemName"));
+        // Add the item to the collection
+        listItems.add(item);
+        // Clear the text box
+        $('#toDo-Item').val('');       
     }
 });
 
-ToDoView = Backbone.View.extend({
-
-    tagName: 'li',
-
+// View for the existing 'Add Button' to attach an event
+// and handle the event
+var ToDoItemInputView = Backbone.View.extend({
+    el:'#toDo-Item',
+    
     events: {
-        'click #button':  'addToDo',
-        'click .item':  'removeItem',
-    },
-
-    initialize: function() {
-        var thisView = this;
-        this.todoslist = new ToDoList;
-        _.bindAll(this, 'render');
-        //alert("binding");
-        this.todoslist.bind("add", function( model ){
-            //alert("hey");
-            thisView.render( model );
-        })
-    },
-
-    addToDo: function() {
-        var todo_item_val = $('#input').val();
-        this.todoslist.add( {todo_item: todo_item_val} );
+      'keyup':'keyPressEventHandler'
     },
     
-    removeItem: function() {
-        //$(this).remove();
+    initialize: function () {
+        console.log("initialize");
     },
-
-    render: function( model ) {
-        $(".list").append("<div class='item'>"+ model.get("todo_item")+"</div>");
-        console.log('rendered')
+    
+    keyPressEventHandler: function(event) {
+       if (event.keyCode == 13) {
+          //alert("YES");
+          //addButtonView.click();
+          //this.addButtonView.click();
+          this.addItem();
+       }    
     },
-
+    
+    addItem: function() {
+      //alert("AddButtonView.addItem");
+        // Get the new item from the text input box using
+        // jQuery function .val()
+        var newItem = $('#toDo-Item').val();
+        //alert("newItem = " + newItem);
+        // Create a new list item model with the new item name
+        var item = new ListItemModel({itemName: newItem});
+        console.log(item.get("itemName"));
+        // Add the item to the collection
+        listItems.add(item);
+        // Clear the text box
+        $('#toDo-Item').val('');       
+    }    
 });
 
-var view = new ToDoView({el: 'body'});
+// Create the initial list
+var ToDoListContainerView = Backbone.View.extend({
+    tagName:'div',
+    
+    id:'todo-list-container',
+    
+    initialize: function() {
+      //alert("ToDoListContainerView.intialize");
+        // Add a listener to the collection which will listen
+        // for any additions to the collection
+        listItems.on('add', this.addItemToList);
+    },
+    
+    render:function() {
+        // Render the initial empty list. No data is passed
+        // because the list is empty.
+        // var source = $('#shopping-list-content').html();
+        // var template = Handlebars.compile(source);
+        // var html = template();
+        
+        // this.$el.html(html);
+        
+        console.log('ToDoListContainerView.rendered');        
+        
+        return this;
+    },
+    // When there's a new additon to the list, render it
+    addItemToList: function() {
+        // Create a new list item
+        var newListItem = listItems.at(listItems.length - 1);
+        var itemToAdd = new ListItemView({model:newListItem});
+      //alert("ToDoListContainerView.addItemToList value = "+ this);
+
+        // Append it to the end of the existing list
+        //$('#lists-of-items').append(itemToAdd.render().el);
+        $('#lists-of-items').append(itemToAdd.render().el);
+        
+        return this;        
+    }
 });
+
+// Create instances of each top level/standalone view and
+// render as appropriate for start of application.
+var addButtonView = new AddButtonView();
+var toDoListContainer = new ToDoListContainerView();
+var ToDoItemInputView = new ToDoItemInputView();
+//$('#list-content').html(toDoListContainer.render().el);
